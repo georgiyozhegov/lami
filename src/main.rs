3b0fc372313bba0021d3f84b6d3230ba333692e4
Georgiy Ozhegov <georgiyozhegov.rs@gmail.com>
2025-09-08 00:20:07 +0400

    Working interpreter

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,171 @@
-use std::{iter::Peekable, vec};
+use std::collections::HashMap;
+use std::iter::Peekable;
+use std::rc::Rc;
+use std::vec;
 
 fn main() {
     let source = std::fs::read_to_string("source.lami").unwrap();
     let tokenized = Lexer::new(&source).lex();
-    let tree = Parser::new(tokenized.into_iter()).parse();
-    for let_ in tree {
-        println!("{let_:#?}");
+    let program = Parser::new(tokenized.into_iter()).parse();
+    let interpreter = Interpreter::new(program.into_iter());
+    let main = interpreter.run();
+    println!("{main:#?}");
+}
+
+struct Interpreter {
+    program: vec::IntoIter<Let>,
+    global: Context,
+}
+
+impl Interpreter {
+    pub fn new(program: impl Iterator<Item = Let>) -> Self {
+        let program: Vec<_> = program.collect();
+        Self {
+            program: program.into_iter(),
+            global: Context::new(),
+        }
+    }
+
+    fn insert_sl(&mut self) {
+        self.global.insert(
+            "plus".into(),
+            Value::Intrinsic(Rc::new(|left| match left {
+                Value::Number(left) => Value::Intrinsic(Rc::new(move |right| match right {
+                    Value::Number(right) => Value::Number(left + right),
+                    _ => panic!("Cannot apply \"plus\" to a non-number argument"),
+                })),
+                _ => panic!("Cannot apply \"plus\" to a non-number argument"),
+            })),
+        );
+
+        self.global.insert(
+            "minus".into(),
+            Value::Intrinsic(Rc::new(|left| match left {
+                Value::Number(left) => Value::Intrinsic(Rc::new(move |right| match right {
+                    Value::Number(right) => Value::Number(left - right),
+                    _ => panic!("Cannot apply \"minus\" to a non-number argument"),
+                })),
+                _ => panic!("Cannot apply \"minus\" to a non-number argument"),
+            })),
+        );
+
+        self.global.insert(
+            "lessThan".into(),
+            Value::Intrinsic(Rc::new(|left| match left {
+                Value::Number(left) => Value::Intrinsic(Rc::new(move |right| match right {
+                    Value::Number(right) => Value::Number((left < right) as i128),
+                    _ => panic!("Cannot apply \"lessThan\" to a non-number argument"),
+                })),
+                _ => panic!("Cannot apply \"lessThan\" to a non-number argument"),
+            })),
+        );
+
+        self.global.insert("_".into(), Value::Number(0));
+    }
+
+    fn run(mut self) -> Value {
+        self.insert_sl();
+        for let_ in self.program {
+            let value = Self::evaluate(let_.value, &mut self.global);
+            self.global.insert(let_.identifier, value);
+        }
+        let main = self
+            .global
+            .get("main")
+            .expect("Missing the \"main\" function")
+            .clone();
+        main
+    }
+
+    fn evaluate(value: Expression, context: &mut Context) -> Value {
+        match value {
+            Expression::Lambda { parameter, body } => Value::Closure {
+                parameter,
+                body: *body,
+                context: context.clone(),
+            },
+            Expression::Application { left, right } => {
+                Self::evaluate_application(*left, *right, context)
+            }
+            Expression::If {
+                condition,
+                then,
+                otherwise,
+            } => Self::evaluate_if(*condition, *then, *otherwise, context),
+            Expression::Parenthesized(inner) => Self::evaluate(*inner, context),
+            Expression::Identifier(name) => match context.get(&name) {
+                Some(value) => value.clone(),
+                _ => panic!("Name {name:?} is not found"),
+            },
+            Expression::Number(raw_value) => match raw_value.parse::<i128>() {
+                Ok(value) => Value::Number(value),
+                _ => panic!("Failed to parse a number literal: {raw_value:?}"),
+            },
+        }
+    }
+
+    fn evaluate_application(left: Expression, right: Expression, context: &mut Context) -> Value {
+        let left = Self::evaluate(left, context);
+        let argument = Self::evaluate(right, context);
+        match left {
+            Value::Closure {
+                parameter,
+                body,
+                context: mut local,
+            } => {
+                local.insert(parameter, argument);
+                Self::evaluate(body, &mut local)
+            }
+            Value::Intrinsic(function) => function(argument),
+            _ => panic!("Cannot apply a non-function: {left:?}"),
+        }
+    }
+
+    fn evaluate_if(
+        condition: Expression,
+        then: Expression,
+        otherwise: Expression,
+        context: &mut Context,
+    ) -> Value {
+        match Self::evaluate(condition, context) {
+            Value::Number(1) => Self::evaluate(then, context),
+            Value::Number(0) => Self::evaluate(otherwise, context),
+            value => panic!("Invalid boolean value: {value:?}"),
+        }
     }
 }
+
+#[derive(Clone)]
+pub enum Value {
+    Number(i128),
+    Closure {
+        parameter: String,
+        body: Expression,
+        context: Context,
+    },
+    Intrinsic(Rc<dyn Fn(Value) -> Value>),
+}
+
+impl std::fmt::Debug for Value {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Self::Number(value) => write!(f, "Number({value})"),
+            Self::Closure {
+                parameter,
+                body,
+                context,
+            } => f
+                .debug_struct("Closure")
+                .field("parameter", parameter)
+                .field("body", body)
+                .field("context", context)
+                .finish(),
+            Self::Intrinsic(_) => write!(f, "Intrinsic"),
+        }
+    }
+}
+
+pub type Context = HashMap<String, Value>;
 
 struct Parser {
     tokenized: Peekable<vec::IntoIter<Token>>,
@@ -132,7 +290,7 @@ pub struct Let {
     value: Expression,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Expression {
     Lambda {
         parameter: String,
