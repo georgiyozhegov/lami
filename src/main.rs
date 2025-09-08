4ff17752ab944d44b0552c72d449299e8371008c
Georgiy Ozhegov <georgiyozhegov.rs@gmail.com>
2025-09-08 17:11:10 +0400

    Rename intrinsics

--- src/main.rs
+++ src/main.rs
@@ -43,9 +43,9 @@ impl Interpreter {
             Value::Intrinsic(Rc::new(|left| match left {
                 Value::Number(left) => Value::Intrinsic(Rc::new(move |right| match right {
                     Value::Number(right) => Value::Number(left - right),
-                    _ => panic!("Cannot apply \"minus\" to a non-number argument"),
+                    _ => panic!("Cannot apply \":-\" to a non-number argument"),
                 })),
-                _ => panic!("Cannot apply \"minus\" to a non-number argument"),
+                _ => panic!("Cannot apply \":-\" to a non-number argument"),
             })),
         );
 
@@ -54,9 +54,9 @@ impl Interpreter {
             Value::Intrinsic(Rc::new(|left| match left {
                 Value::Number(left) => Value::Intrinsic(Rc::new(move |right| match right {
                     Value::Number(right) => Value::Number((left < right) as i128),
-                    _ => panic!("Cannot apply \"lessThan\" to a non-number argument"),
+                    _ => panic!("Cannot apply \":<\" to a non-number argument"),
                 })),
-                _ => panic!("Cannot apply \"lessThan\" to a non-number argument"),
+                _ => panic!("Cannot apply \":<\" to a non-number argument"),
             })),
         );
 
